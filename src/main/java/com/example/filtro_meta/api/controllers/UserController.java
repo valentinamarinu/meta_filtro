edc73a445b73a6b88ee8d552e2c44f36fc4383cb
Ana Valentina Marín Usuga <anav.usuga@gmail.com>
2024-06-04 22:02:38 -0500

    Task 01

--- src/main/java/com/example/filtro_meta/api/controllers/UserController.java
+++ src/main/java/com/example/filtro_meta/api/controllers/UserController.java
@@ -1,11 +1,17 @@
 package com.example.filtro_meta.api.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.filtro_meta.api.dto.response.UserResp;
 import com.example.filtro_meta.infrastructure.abstract_services.IUserService;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 
 @RestController
@@ -15,4 +21,11 @@ public class UserController {
     @Autowired
     private final IUserService service;
 
+    @Operation(summary = "Lista todos los usuarios con paginación", description = "Debes enviar la página y el tamaño de la página para recibir todas los usuarios correspondientes")
+    @GetMapping(path = "/class")
+    public ResponseEntity<Page<UserResp>> getAll(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "5") int size) {
+        return ResponseEntity.ok(this.service.getAll(page - 1, size));
+    }
 }
--- src/main/java/com/example/filtro_meta/infrastructure/services/SurveyService.java
+++ src/main/java/com/example/filtro_meta/infrastructure/services/SurveyService.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import com.example.filtro_meta.api.dto.request.SurveyReq;
@@ -34,8 +35,13 @@ public class SurveyService implements ISurveyService {
     
     @Override
     public Page<SurveyResp> getAll(int page, int size) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAll'");
+        if (page < 0)
+            page = 0;
+
+        PageRequest pagination = PageRequest.of(page, size);
+
+        return this.repository.findAll(pagination)
+                .map(user -> this.entityToResponse(user)); 
     }
 
     @Override
